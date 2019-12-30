fb598b64ae10be1a51a1b9cdb8995f2879f9d00c
rhyso88 <rhyso88@hotmail.com>
2019-12-30 20:35:08 +0800

    installed npm webpack latest (which removed its error), then tried installing serialise-javascript & atob to remove vulnerabilities (this didn't work). I also installed sqllite plugin to try and remove warning, however the issue still exists. I will try and fix the sqllite-commercial issue by editing webpakc file as described by ludwig on stackoverflow

--- app/main.js
+++ app/main.js
@@ -2,27 +2,6 @@ import Vue from "nativescript-vue";
 import routes from "./routes";
 import BackendService from "./services/backend-service";
 
-import * as Kinvey from "kinvey-nativescript-sdk";
-
-/*
-Kinvey.init({
-    appKey: "kid_S1Gok7ARr",
-    appSecret: "430e2c4f39c240e6970bd5683bf9cc09"
-});
-
-
-Kinvey.ping()
-    .then(function(response) {
-        console.log(
-            'Kinvey Ping Success. Kinvey Service is alive, version: '
-            + response.version + ', response: ' + response.kinvey);
-    })
-    .catch(function(error) {
-        console.log('Kinvey Ping Failed. Response: ' + error.description);
-    });
-
-    */
-
 // Uncommment the following to see NativeScript-Vue output logs
 //Vue.config.silent = false;
 
